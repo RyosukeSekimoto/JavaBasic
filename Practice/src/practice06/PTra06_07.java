0df188363a2b9c3a556a93acfa0e55570d4ffbec
ryosuke sekimoto <shojiniyan@gmail.com>
2017-11-13 21:14:48 +0900

    Ptra06_07 まで完了

--- Practice/src/practice06/PTra06_07.java
+++ Practice/src/practice06/PTra06_07.java
@@ -24,6 +24,25 @@ public class PTra06_07 {
 		 * ■■□□□
 		 * ■□□□□
 		 */
-
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 5; j++) {
+				//iが5までの処理
+				if(i <= 5) {
+					if(j < i) {
+						System.out.print("■");
+					} else {
+						System.out.print("□");
+					}
+				//iが5より大きいときの処理
+				} else if(i > 5) {
+					if(j < 10 - i) {
+						System.out.print("■");
+					} else {
+						System.out.print("□");
+					}
+				}
+			}
+			System.out.println("");
+		}
 	}
 }
